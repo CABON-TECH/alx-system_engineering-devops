081204026388b2c0e97ebf04ed6e81fdeb13ee44
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-03-29 13:13:44 -0700

    updated readme

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,5 +0,0 @@
-# Using puppet to change SSH config file
-exec { 'echo':
-  path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/school\n   PasswordAuthentication no" >> /etc/ssh/ssh_config'
-}
